a15d1bbb7835e00884db8f9b4165451a9e9dba91
Sam Wang <sam.zz.wang@gmail.com>
2017-06-14 11:50:16 -0700

    Complete p07_practical

--- sam_wang/lib/p07_practical.rb
+++ sam_wang/lib/p07_practical.rb
@@ -1,5 +1,13 @@
 require_relative 'p05_hash_map'
 
-def can_string_be_palindrome?(string) 
-  
+def can_string_be_palindrome?(string)
+  letter_cnt = Hash.new(0)
+  string.each_char { |chr| letter_cnt[chr] += 1 }
+
+  if string.length % 2 == 1
+    letter_cnt.select { |_, v| v == 1 }.keys.length == 1 &&
+    letter_cnt.select { |_, v| v == 2 }.keys.length == string.length / 2
+  elsif string.length % 2 == 0
+    letter_cnt.select { |_, v| v == 2 }.keys.length == string.length
+  end
 end
