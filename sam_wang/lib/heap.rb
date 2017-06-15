bf4144c99d1923a788230356019878f6253da508
Sam Wang <sam.zz.wang@gmail.com>
2017-06-15 10:06:43 -0700

    Implement heap

--- sam_wang/lib/heap.rb
+++ sam_wang/lib/heap.rb
@@ -11,12 +11,25 @@ class BinaryMinHeap
   end
 
   def extract
+    val = store[0]
+
+    if count > 1
+      store[0] = store.pop
+      self.class.heapify_down(store, 0, &prc)
+    else
+      store.pop
+    end
+
+    val
   end
 
   def peek
+    @store[0]
   end
 
   def push(val)
+    store << val
+    self.class.heapify_up(store, self.count - 1, &prc)
   end
 
   public
@@ -27,12 +40,46 @@ class BinaryMinHeap
   end
 
   def self.parent_index(child_index)
-    (child_idex - 1) / 2
+    raise "root has no parent" if child_index == 0
+    (child_index - 1) / 2
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+    prc ||= Proc.new { |a, b| a <=> b }
+
+    parent_val = array[parent_idx]
+
+    l_child_idx, r_child_idx = child_indices(len, parent_idx)
+
+    children = []
+    children << array[l_child_idx] if l_child_idx
+    children << array[r_child_idx] if r_child_idx
+
+    return array if children.all? { |child| prc.call(child, parent_val) >= 0 }
+
+    swap_idx = nil
+    if children.length == 1
+      swap_idx = l_child_idx
+    else
+      swap_idx = prc.call(array[l_child_idx], array[r_child_idx]) == -1 ? l_child_idx : r_child_idx
+    end
+
+    array[parent_idx], array[swap_idx] = array[swap_idx], array[parent_idx]
+    heapify_down(array, swap_idx, len, &prc)
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
+    prc ||= Proc.new { |a, b| a <=> b }
+
+    return array if child_idx == 0
+
+    parent_idx = parent_index(child_idx)
+    parent_val, child_val = array[parent_idx], array[child_idx]
+    if prc.call(parent_val, child_val) <= 0
+      return array
+    else
+      array[parent_idx], array[child_idx] = child_val, parent_val
+      heapify_up(array, parent_idx, len, &prc)
+    end
   end
 end
