c054ceabff0f869152b8ca917dbf8c7a6d784b46
Sam Wang <sam.zz.wang@gmail.com>
2017-06-17 15:22:57 -0700

    Implement #insert

--- binary_search_trees/sam_wang/lib/binary_search_tree.rb
+++ binary_search_trees/sam_wang/lib/binary_search_tree.rb
@@ -1,11 +1,21 @@
-# There are many ways to implement these methods, feel free to add arguments 
+# There are many ways to implement these methods, feel free to add arguments
 # to methods as you see fit, or to create helper methods.
+require_relative 'bst_node'
 
 class BinarySearchTree
+  attr_accessor :root
+
   def initialize
+    @root = nil
   end
 
   def insert(value)
+    if !@root
+      @root = BSTNode.new(value)
+      return
+    end
+
+    BinarySearchTree.insert!(@root, value)
   end
 
   def find(value, tree_node = @root)
@@ -19,7 +29,7 @@ class BinarySearchTree
   end
 
   def depth(tree_node = @root)
-  end 
+  end
 
   def is_balanced?(tree_node = @root)
   end
@@ -30,5 +40,15 @@ class BinarySearchTree
 
   private
   # optional helper methods go here:
+  def self.insert!(node, value)
+    return BSTNode.new(value) unless node
 
+    if value <= node.value
+      node.left = BinarySearchTree.insert!(node.left, value)
+    else
+      node.right = BinarySearchTree.insert!(node.right, value)
+    end
+
+    node
+  end
 end
