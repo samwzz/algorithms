c52f78d34606995bd4dbadcf479b9e78207e314d
Sam Wang <sam.zz.wang@gmail.com>
2017-06-18 22:53:06 -0700

    Complete bst practical

--- binary_search_trees/sam_wang/lib/bst_practical.rb
+++ binary_search_trees/sam_wang/lib/bst_practical.rb
@@ -1,3 +1,17 @@
+require_relative 'bst_practical'
 
 def kth_largest(tree_node, k)
+  arr = in_order_traversal(tree_node)
+  arr[arr.length - k]
+end
+
+def in_order_traversal(tree_node)
+  return [] unless tree_node
+
+  arr = []
+  arr.concat(in_order_traversal(tree_node.left)) if tree_node.left
+  arr << tree_node
+  arr.concat(in_order_traversal(tree_node.right)) if tree_node.right
+
+  arr
 end
