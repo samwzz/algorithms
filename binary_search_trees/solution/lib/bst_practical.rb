96fd563caa130b52356a3abd9a50c0bde70aa2e7
Sam Wang <sam.zz.wang@gmail.com>
2017-06-19 14:16:44 -0700

    Add time complexity of solution

--- binary_search_trees/solution/lib/bst_practical.rb
+++ binary_search_trees/solution/lib/bst_practical.rb
@@ -1,14 +1,15 @@
+# O(depth + k)
 def kth_largest(tree_node, k)
   kth_node = { count: 0, correct_node: nil }
   reverse_inorder(tree_node, kth_node, k)[:correct_node]
 end
 
 def reverse_inorder(tree_node, kth_node, k)
-  if tree_node && kth_node[:count] < k 
+  if tree_node && kth_node[:count] < k
     kth_node = reverse_inorder(tree_node.right, kth_node, k)
-    if kth_node[:count] < k 
+    if kth_node[:count] < k
       kth_node[:count] += 1
-      kth_node[:correct_node] = tree_node 
+      kth_node[:correct_node] = tree_node
     end
 
     if kth_node[:count] < k
