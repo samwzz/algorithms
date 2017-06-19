ee5e89d3b2240d2e8464a32c9664a0c73b7b6233
Sam Wang <sam.zz.wang@gmail.com>
2017-06-18 22:11:56 -0700

    Implement #delete

--- binary_search_trees/sam_wang/lib/binary_search_tree.rb
+++ binary_search_trees/sam_wang/lib/binary_search_tree.rb
@@ -23,13 +23,20 @@ class BinarySearchTree
   end
 
   def delete(value)
+    if value == @root.value
+      @root = nil
+    else
+      BinarySearchTree.delete!(@root, value)
+    end
   end
 
   # helper method for #delete:
   def maximum(tree_node = @root)
+    BinarySearchTree.max(tree_node)
   end
 
   def depth(tree_node = @root)
+    BinarySearchTree.depth(tree_node)
   end
 
   def is_balanced?(tree_node = @root)
@@ -62,5 +69,37 @@ class BinarySearchTree
     else
       BinarySearchTree.find!(node.right, value)
     end
+  end
+
+  def self.max(node)
+    return node unless node.right
+
+    BinarySearchTree.max(node.right)
+  end
+
+  def self.delete_max!(node)
+    return node.left unless node.right
+
+    node.right = BinarySearchTree.delete_max!(node.right)
+    node
+  end
+
+  def self.delete!(node, value)
+    return nil unless node
+
+    if value < node.value
+      node.left = BinarySearchTree.delete!(node.left, value)
+    elsif value > node.value
+      node.right = BinarySearchTree.delete!(node.right, value)
+    else
+      return node.left unless node.right
+      return node.right unless node.left
+      target_node = node
+      node = BinarySearchTree.max(target_node.left)
+      node.left = BinarySearchTree.delete_max!(target_node.left)
+      node.right = target_node.right
+    end
+
+    node
   end
 end
